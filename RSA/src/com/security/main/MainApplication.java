f085a545f27219c407ef32f03fb45e7113badf57
momazia@gmail.com <momazia@gmail.com>
2016-06-30 19:21:08 -0700

    - Added input to the program - Changed integer to big integer

--- RSA/src/com/security/main/MainApplication.java
+++ RSA/src/com/security/main/MainApplication.java
@@ -1,9 +1,56 @@
 package com.security.main;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+import com.security.util.RSA;
+
+/**
+ * Main application to be executed for running RSA encryption
+ * 
+ * @author Max
+ *
+ */
 public class MainApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+		System.out.println("Do you wish to auto generate P and Q? [Y/n]");
+		String autoPQAnswer = br.readLine();
+		RSA rsa = null;
+
+		if (autoGeneratePQ(autoPQAnswer)) {
+
+			System.out.println("Enter a minimun number (inclusive) for P and Q:");
+			String minNumber = br.readLine();
+
+			System.out.println("Enter a maximum number (exclusive) for P and Q: (This is to avoid very big numbers)");
+			String maxNumber = br.readLine();
+
+			rsa = new RSA(Integer.valueOf(minNumber), Integer.valueOf(maxNumber));
+		} else {
+
+			System.out.println("Enter P:");
+			String p = br.readLine();
+
+			System.out.println("Enter Q:");
+			String q = br.readLine();
+
+			rsa = new RSA(BigInteger.valueOf(Long.valueOf(p)), BigInteger.valueOf(Long.valueOf(q)));
+		}
+		System.out.println("P [" + rsa.getP() + "], Q[" + rsa.getQ() + "], N[" + rsa.getN() + "], Z[" + rsa.getZ() + "], E[" + rsa.getE() + "], D[" + rsa.getD() + "]");
+
+		System.out.println("Enter your message:");
+		String message = br.readLine();
+
+		Integer[] encryptedText = rsa.encrypt(message);
+	}
+
+	private static boolean autoGeneratePQ(String autoPQAnswer) {
+		return !"n".equals(autoPQAnswer);
 	}
 
 }
--- RSA/src/com/security/util/RSA.java
+++ RSA/src/com/security/util/RSA.java
@@ -11,25 +11,45 @@ import java.util.Random;
  */
 public class RSA {
 
-	private Integer p;
-	private Integer q;
+	private BigInteger p;
+	private BigInteger q;
 
-	private Integer n;
-	private Integer z;
+	private BigInteger n;
+	private BigInteger z;
 
-	private Integer e;
-	private Integer d;
+	private BigInteger e;
+	private BigInteger d;
 
-	public RSA(int p, int q) {
+	/**
+	 * Using the given P and Q, it prepares the encryption by calculating the necessary fields.
+	 * 
+	 * @param p
+	 * @param q
+	 */
+	public RSA(BigInteger p, BigInteger q) {
 		this.p = p;
 		this.q = q;
+		calculateN();
+		calculateZ();
+		calculateE();
+		calculateD();
+	}
+
+	public RSA() {
+
 	}
 
 	/**
-	 * Default constructor.
+	 * Generates two random P and Q which are more than minimum number passed. It also prepares the encryption by calculating the necessary fields.
+	 * 
+	 * @param minNumber
 	 */
-	public RSA() {
-
+	public RSA(Integer minNumber, Integer maxNumber) {
+		generatePQ(minNumber, maxNumber);
+		calculateN();
+		calculateZ();
+		calculateE();
+		calculateD();
 	}
 
 	/**
@@ -37,11 +57,27 @@ public class RSA {
 	 */
 	public void calculateE() {
 		boolean hasCommonFactor = true;
-		Random random = new Random();
+		BigInteger num_2 = BigInteger.valueOf(2);
+		BigInteger neg_num_2 = BigInteger.valueOf(-2);
 		while (hasCommonFactor) {
-			setE(random.nextInt(n - 2) + 2); // To make sure it is bigger than 2, smaller than n
-			hasCommonFactor = haveCommonFactor(getE(), z);
+			e = getRandomBigInteger(n.add(neg_num_2)).add(num_2); // To make sure it is bigger than 2, smaller than n
+			hasCommonFactor = haveCommonFactor(e, z);
 		}
+	}
+
+	/**
+	 * Returns a random number which is smaller than max given.
+	 * 
+	 * @param max
+	 * @return
+	 */
+	public BigInteger getRandomBigInteger(BigInteger max) {
+		Random rnd = new Random();
+		do {
+			BigInteger i = new BigInteger(max.bitLength(), rnd);
+			if (i.compareTo(max) < 0)
+				return i;
+		} while (true);
 	}
 
 	/**
@@ -51,14 +87,14 @@ public class RSA {
 	 * @param b
 	 * @return
 	 */
-	public boolean haveCommonFactor(Integer a, Integer b) {
+	public boolean haveCommonFactor(BigInteger a, BigInteger b) {
 		// If the numbers are the same, then they have common factors
-		if (a == b) {
+		if (a.compareTo(b) == 0) {
 			return true;
 		}
-		int max = a > b ? a : b;
-		for (int i = 2; i <= max / 2; i++) {
-			if (a % i == 0 && b % i == 0) {
+		BigInteger max = a.compareTo(b) > 0 ? a : b;
+		for (BigInteger i = BigInteger.valueOf(2); i.compareTo(max.divide(BigInteger.valueOf(2))) <= 0; i = i.add(BigInteger.ONE)) {
+			if (a.mod(i).compareTo(BigInteger.ZERO) == 0 && b.mod(i).compareTo(BigInteger.ZERO) == 0) {
 				return true;
 			}
 		}
@@ -70,41 +106,46 @@ public class RSA {
 	 * 
 	 * @param minNumber
 	 */
-	public void generatePQ(int minNumber) {
-		Integer randInt1 = generateRandomNumberInbound(minNumber);
-		Integer randInt2 = generateRandomNumberInbound(minNumber);
-		this.p = generatePrimeNumber(randInt1);
-		this.q = generatePrimeNumber(randInt2);
+	public void generatePQ(int minNumber, int maxNumber) {
+		Integer randInt1 = generateRandomNumberInbound(minNumber, maxNumber);
+		Integer randInt2 = generateRandomNumberInbound(minNumber, maxNumber);
+		this.p = BigInteger.valueOf(generatePrimeNumber(randInt1));
+		this.q = BigInteger.valueOf(generatePrimeNumber(randInt2));
 	}
 
 	/**
 	 * Calculates n = p * q
 	 */
 	public void calculateN() {
-		n = p * q;
+		n = p.multiply(q);
 	}
 
 	/**
 	 * Calculates z = (p - 1) * (q - 1)
 	 */
 	public void calculateZ() {
-		z = (p - 1) * (q - 1);
+		BigInteger p_1 = p.add(BigInteger.valueOf(-1));
+		BigInteger q_1 = q.add(BigInteger.valueOf(-1));
+		z = p_1.multiply(q_1);
 	}
 
 	/**
 	 * Calculates D using Modular multiplicative inverse
 	 */
 	public void calculateD() {
-		d = BigInteger.valueOf(getE()).modInverse(BigInteger.valueOf(z)).intValue();
+		d = e.modInverse(z);
 	}
 
 	/**
-	 * Generates a random number between minNumber and Integer.MAX_VALUE
+	 * Generates a random number between minNumber and maxNumber
 	 * 
 	 * @param minNumber
+	 *            inclusive
+	 * @param maxNumber
+	 *            exclusive
 	 */
-	public Integer generateRandomNumberInbound(int minNumber) {
-		Integer difference = Integer.MAX_VALUE - minNumber - 1;
+	public Integer generateRandomNumberInbound(int minNumber, int maxNumber) {
+		Integer difference = maxNumber - minNumber - 1;
 		Random random = new Random();
 		int randomInt = random.nextInt(difference);
 		return randomInt + minNumber;
@@ -118,7 +159,7 @@ public class RSA {
 	 */
 	public Integer generatePrimeNumber(int minNumber) {
 		for (int i = minNumber; i < Integer.MAX_VALUE; i++) {
-			if (isPrime(i)) {
+			if (isPrime(BigInteger.valueOf(i))) {
 				return i;
 			}
 		}
@@ -131,49 +172,55 @@ public class RSA {
 	 * @param n
 	 * @return
 	 */
-	public boolean isPrime(int n) {
+	public boolean isPrime(BigInteger n) {
 		// Handling the only even prime number
-		if (n == 2) {
+		BigInteger num_2 = BigInteger.valueOf(2);
+		if (n.compareTo(num_2) == 0) {
 			return true;
 		}
 		// check if n is a multiple of 2, this way we eliminate 50% of the
 		// numbers
-		if (n % 2 == 0) {
+		if (n.mod(num_2).compareTo(BigInteger.ZERO) == 0) {
 			return false;
 		}
-		for (int i = 3; i * i <= n; i += 2) {
-			if (n % i == 0) {
+		for (BigInteger i = BigInteger.valueOf(3); i.multiply(i).compareTo(n) <= 0; i = i.add(num_2)) {
+			if (n.mod(i).compareTo(BigInteger.ZERO) == 0) {
 				return false;
 			}
 		}
 		return true;
 	}
 
-	public Integer getN() {
+	public Integer[] encrypt(String string) {
+		return null;
+	}
+
+	public BigInteger getN() {
 		return n;
 	}
 
-	public Integer getZ() {
+	public BigInteger getZ() {
 		return z;
 	}
 
-	public Integer getE() {
+	public BigInteger getE() {
 		return e;
 	}
 
-	public Integer getD() {
+	public BigInteger getD() {
 		return d;
 	}
 
-	public void setE(Integer e) {
+	public void setE(BigInteger e) {
 		this.e = e;
 	}
 
-	public Integer getP() {
+	public BigInteger getP() {
 		return p;
 	}
 
-	public Integer getQ() {
+	public BigInteger getQ() {
 		return q;
 	}
+
 }
