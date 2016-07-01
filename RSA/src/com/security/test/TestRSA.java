9cadab3b2fd03d8220cca8c2047e2cb1aba6bc89
momazia@gmail.com <momazia@gmail.com>
2016-06-30 17:39:17 -0700

    Completed RSA calculations

--- RSA/src/com/security/test/TestRSA.java
+++ RSA/src/com/security/test/TestRSA.java
@@ -1,6 +1,10 @@
 package com.security.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
 
 import org.junit.Test;
 
@@ -9,19 +13,94 @@ import com.security.util.RSA;
 public class TestRSA {
 
 	@Test
-	public void testGeneratePQ() {
+	public void testGeneratePrimeNumber() {
 		RSA rsa = new RSA();
-		rsa.generatePQ();
-		assertTrue("Generated P" + rsa.getP(), isPrime(rsa.getP()));
+		Integer primeNumber = rsa.generatePrimeNumber(2);
+		assertEquals(Integer.valueOf(2), primeNumber);
+		primeNumber = rsa.generatePrimeNumber(3);
+		assertEquals(Integer.valueOf(3), primeNumber);
+		primeNumber = rsa.generatePrimeNumber(4);
+		assertEquals(Integer.valueOf(5), primeNumber);
+		primeNumber = rsa.generatePrimeNumber(5);
+		assertEquals(Integer.valueOf(5), primeNumber);
+		primeNumber = rsa.generatePrimeNumber(6);
+		assertEquals(Integer.valueOf(7), primeNumber);
+		primeNumber = rsa.generatePrimeNumber(7);
+		assertEquals(Integer.valueOf(7), primeNumber);
 	}
 
-	private boolean isPrime(int n) {
-		for (int i = 2; i < n; i++) {
-			if (n % i == 0) {
-				return false;
-			}
-		}
-		return true;
+	@Test
+	public void testIsPrime() {
+		assertTrue(new RSA().isPrime(2));
+		assertTrue(new RSA().isPrime(3));
+		assertFalse(new RSA().isPrime(4));
+		assertTrue(new RSA().isPrime(5));
+		assertFalse(new RSA().isPrime(6));
+		assertTrue(new RSA().isPrime(7));
+		assertFalse(new RSA().isPrime(8));
+		assertFalse(new RSA().isPrime(9));
+		assertFalse(new RSA().isPrime(10));
+		assertTrue(new RSA().isPrime(11));
+		assertFalse(new RSA().isPrime(12));
+		assertTrue(new RSA().isPrime(13));
+	}
+
+	@Test
+	public void testGenerateRandomNumberInbound() {
+		RSA rsa = new RSA();
+		Integer randomNumber = rsa.generateRandomNumberInbound(4);
+		assertTrue(randomNumber > 4 && randomNumber < Integer.MAX_VALUE);
+	}
+
+	@Test
+	public void testGeneratePQ() {
+		RSA rsa = new RSA();
+		rsa.generatePQ(1000000);
+		assertTrue(rsa.isPrime(rsa.getP()));
+		assertTrue(rsa.isPrime(rsa.getQ()));
+	}
+
+	@Test
+	public void testCalculateN() {
+		RSA rsa = new RSA(3, 5);
+		rsa.calculateN();
+		assertEquals(Integer.valueOf(15), rsa.getN());
+	}
+
+	@Test
+	public void testCalculateZ() {
+		RSA rsa = new RSA(3, 5);
+		rsa.calculateZ();
+		assertEquals(Integer.valueOf(8), rsa.getZ());
+	}
+
+	@Test
+	public void testCalculateE() {
+		RSA rsa = new RSA(3, 5);
+		rsa.calculateN();
+		rsa.calculateZ();
+		rsa.calculateE();
+		assertTrue("Wrong value: " + rsa.getE(), Arrays.asList(new Integer[] { 3, 5, 7, 9, 11, 13 }).contains(rsa.getE()));
+	}
+
+	@Test
+	public void testHaveCommonFactor() {
+		assertTrue(new RSA().haveCommonFactor(5, 5));
+		assertFalse(new RSA().haveCommonFactor(5, 6));
+		assertTrue(new RSA().haveCommonFactor(10, 6));
+		assertFalse(new RSA().haveCommonFactor(11, 60));
+		assertFalse(new RSA().haveCommonFactor(17, 43));
+		assertFalse(new RSA().haveCommonFactor(7, 8));
+	}
+
+	@Test
+	public void testCalculateD() {
+		RSA rsa = new RSA(11, 5);
+		rsa.calculateN();
+		rsa.calculateZ();
+		rsa.setE(7);
+		rsa.calculateD();
+		assertEquals(Integer.valueOf(23), rsa.getD());
 	}
 
 }
--- RSA/src/com/security/util/RSA.java
+++ RSA/src/com/security/util/RSA.java
@@ -1,40 +1,179 @@
 package com.security.util;
 
+import java.math.BigInteger;
+import java.util.Random;
+
+/**
+ * RSA encryption class
+ * 
+ * @author Max
+ *
+ */
 public class RSA {
 
 	private Integer p;
 	private Integer q;
+
+	private Integer n;
 	private Integer z;
+
 	private Integer e;
 	private Integer d;
 
 	public RSA(int p, int q) {
-		this.setP(p);
-		this.setQ(q);
+		this.p = p;
+		this.q = q;
 	}
 
+	/**
+	 * Default constructor.
+	 */
 	public RSA() {
 
+	}
+
+	/**
+	 * Assigns a random number to E which is less than n and has no common factor with z.
+	 */
+	public void calculateE() {
+		boolean hasCommonFactor = true;
+		Random random = new Random();
+		while (hasCommonFactor) {
+			setE(random.nextInt(n - 2) + 2); // To make sure it is bigger than 2, smaller than n
+			hasCommonFactor = haveCommonFactor(getE(), z);
+		}
+	}
+
+	/**
+	 * Checks to see if the numbers given have common factors.
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public boolean haveCommonFactor(Integer a, Integer b) {
+		// If the numbers are the same, then they have common factors
+		if (a == b) {
+			return true;
+		}
+		int max = a > b ? a : b;
+		for (int i = 2; i <= max / 2; i++) {
+			if (a % i == 0 && b % i == 0) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Generates two random P and Q which are higher than the min number passed.
+	 * 
+	 * @param minNumber
+	 */
+	public void generatePQ(int minNumber) {
+		Integer randInt1 = generateRandomNumberInbound(minNumber);
+		Integer randInt2 = generateRandomNumberInbound(minNumber);
+		this.p = generatePrimeNumber(randInt1);
+		this.q = generatePrimeNumber(randInt2);
+	}
+
+	/**
+	 * Calculates n = p * q
+	 */
+	public void calculateN() {
+		n = p * q;
+	}
+
+	/**
+	 * Calculates z = (p - 1) * (q - 1)
+	 */
+	public void calculateZ() {
+		z = (p - 1) * (q - 1);
+	}
+
+	/**
+	 * Calculates D using Modular multiplicative inverse
+	 */
+	public void calculateD() {
+		d = BigInteger.valueOf(getE()).modInverse(BigInteger.valueOf(z)).intValue();
+	}
+
+	/**
+	 * Generates a random number between minNumber and Integer.MAX_VALUE
+	 * 
+	 * @param minNumber
+	 */
+	public Integer generateRandomNumberInbound(int minNumber) {
+		Integer difference = Integer.MAX_VALUE - minNumber - 1;
+		Random random = new Random();
+		int randomInt = random.nextInt(difference);
+		return randomInt + minNumber;
+	}
+
+	/**
+	 * Generate a Prime number with a minimum number to start with
+	 * 
+	 * @param minNumber
+	 * @return
+	 */
+	public Integer generatePrimeNumber(int minNumber) {
+		for (int i = minNumber; i < Integer.MAX_VALUE; i++) {
+			if (isPrime(i)) {
+				return i;
+			}
+		}
+		throw new IllegalArgumentException("There is no prime number after minNumber [" + minNumber + "] passed");
+	}
+
+	/**
+	 * Checks to see if the given number is prime or not.
+	 * 
+	 * @param n
+	 * @return
+	 */
+	public boolean isPrime(int n) {
+		// Handling the only even prime number
+		if (n == 2) {
+			return true;
+		}
+		// check if n is a multiple of 2, this way we eliminate 50% of the
+		// numbers
+		if (n % 2 == 0) {
+			return false;
+		}
+		for (int i = 3; i * i <= n; i += 2) {
+			if (n % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public Integer getN() {
+		return n;
+	}
+
+	public Integer getZ() {
+		return z;
+	}
+
+	public Integer getE() {
+		return e;
+	}
+
+	public Integer getD() {
+		return d;
+	}
+
+	public void setE(Integer e) {
+		this.e = e;
 	}
 
 	public Integer getP() {
 		return p;
 	}
 
-	public void setP(Integer p) {
-		this.p = p;
-	}
-
 	public Integer getQ() {
 		return q;
 	}
-
-	public void setQ(Integer q) {
-		this.q = q;
-	}
-	
-	public void generatePQ(){
-		
-	}
-
 }
